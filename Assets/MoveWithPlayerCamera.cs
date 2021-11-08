84fe0189bffc4dcc2c3eac359d2b242243ef95ae
Patrick Burns <pwb0007@uah.edu>
2021-11-08 14:19:00 -0600

    added test files

---
+++ Assets/MoveWithPlayerCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWithPlayerCamera : MonoBehaviour
+{
+    [Header("Inspector - Set Values")]
+    public GameObject player;
+    public float linearInterpolant;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 playerPosition;
+        float yPosition;
+        yPosition = this.transform.position.y;
+
+        playerPosition = Vector3.Lerp(this.transform.position, player.transform.position, linearInterpolant);
+        playerPosition.y = yPosition;
+
+        
+
+        this.transform.position = playerPosition;
+    }
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [Header("Inspector - Set Values")]
+    public float movementSpeed;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 position;
+        if (Input.GetKey("w"))
+        { 
+            position = this.transform.position;
+            position.z += movementSpeed;
+            this.transform.position = position;
+        }
+        if (Input.GetKey("a"))
+        {
+            position = this.transform.position;
+            position.x -= movementSpeed;
+            this.transform.position = position;
+        }
+        if (Input.GetKey("s"))
+        {
+            position = this.transform.position;
+            position.z -= movementSpeed;
+            this.transform.position = position;
+        }
+        if (Input.GetKey("d"))
+        {
+            position = this.transform.position;
+            position.x += movementSpeed;
+            this.transform.position = position;
+        }
+        
+    }
+}
